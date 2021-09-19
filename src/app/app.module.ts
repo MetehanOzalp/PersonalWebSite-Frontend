84e4b65c514675967da6cdd8241e7354c57a03b8
Metehan Özalp <metehanozalp@hotmail.com>
2021-09-20 00:41:37 +0300

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { HomeComponent } from './components/home/home.component';
+import { SkillComponent } from './components/skill/skill.component';
+import { ProjectComponent } from './components/project/project.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    HomeComponent,
+    SkillComponent,
+    ProjectComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
